057c426180190b92357b0fb8c93e9ae51a6b793a
GrazielaRO <grazi375@hotmail.com>
2020-10-21 23:10:22 -0300

    projeto os api

---
+++ src/main/java/br/com/osdemo/OsdemoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.osdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsdemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsdemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/osdemo/OsdemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.osdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsdemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
